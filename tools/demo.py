bd1dc86f1e5b9c9168c998bd357ec6d42cd5d9f1
Enhao Gong <gongenhao@gmail.com>
2016-04-20 15:33:19 -0700

    Update demo.py
    update visualization function

--- tools/demo.py
+++ tools/demo.py
@@ -42,34 +42,38 @@ NETS = {'vgg16': ('VGG16',
 def vis_detections(im, class_name, dets, thresh=0.5):
     """Draw detected bounding boxes."""
     inds = np.where(dets[:, -1] >= thresh)[0]
-    if len(inds) == 0:
-        return
+    dets = dets[inds, :]
+    if dets.shape[0]>0:
+        print(class_name,dets)
+#     inds = np.where(dets[:, -1] >= thresh)[0]
+#     if len(inds) == 0:
+#         return
 
-    im = im[:, :, (2, 1, 0)]
-    fig, ax = plt.subplots(figsize=(12, 12))
-    ax.imshow(im, aspect='equal')
-    for i in inds:
-        bbox = dets[i, :4]
-        score = dets[i, -1]
+#     im = im[:, :, (2, 1, 0)]
+#     fig, ax = plt.subplots(figsize=(12, 12))
+#     ax.imshow(im, aspect='equal')
+#     for i in inds:
+#         bbox = dets[i, :4]
+#         score = dets[i, -1]
 
-        ax.add_patch(
-            plt.Rectangle((bbox[0], bbox[1]),
-                          bbox[2] - bbox[0],
-                          bbox[3] - bbox[1], fill=False,
-                          edgecolor='red', linewidth=3.5)
-            )
-        ax.text(bbox[0], bbox[1] - 2,
-                '{:s} {:.3f}'.format(class_name, score),
-                bbox=dict(facecolor='blue', alpha=0.5),
-                fontsize=14, color='white')
+#         ax.add_patch(
+#             plt.Rectangle((bbox[0], bbox[1]),
+#                           bbox[2] - bbox[0],
+#                           bbox[3] - bbox[1], fill=False,
+#                           edgecolor='red', linewidth=3.5)
+#             )
+#         ax.text(bbox[0], bbox[1] - 2,
+#                 '{:s} {:.3f}'.format(class_name, score),
+#                 bbox=dict(facecolor='blue', alpha=0.5),
+#                 fontsize=14, color='white')
 
-    ax.set_title(('{} detections with '
-                  'p({} | box) >= {:.1f}').format(class_name, class_name,
-                                                  thresh),
-                  fontsize=14)
-    plt.axis('off')
-    plt.tight_layout()
-    plt.draw()
+#     ax.set_title(('{} detections with '
+#                   'p({} | box) >= {:.1f}').format(class_name, class_name,
+#                                                   thresh),
+#                   fontsize=14)
+#     plt.axis('off')
+#     plt.tight_layout()
+#     plt.draw()
 
 def demo(net, image_name):
     """Detect object classes in an image using pre-computed object proposals."""
@@ -99,8 +103,7 @@ def demo(net, image_name):
         dets = dets[keep, :]
         inds = np.where(dets[:, -1] >= CONF_THRESH)[0]
         dets = dets[inds, :]
-        print(cls, dets)
-        #vis_detections(im, cls, dets, thresh=CONF_THRESH)
+        vis_detections(im, cls, dets, thresh=CONF_THRESH)
 
 def parse_args():
     """Parse input arguments."""
